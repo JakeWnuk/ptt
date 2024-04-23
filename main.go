fcadebe543ded31096eca836ec03472f5fd7b73f
Jake Wnuk <jakewnuk@gmail.com>
2024-04-23 18:02:44 -0400

    Increment minor version
    - increment minor version for release

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/jakewnuk/ptt/pkg/utils"
 )
 
-var version = "0.1.0"
+var version = "0.1.1"
 var wg sync.WaitGroup
 var mutex = &sync.Mutex{}
 var retain models.FileArgumentFlag
